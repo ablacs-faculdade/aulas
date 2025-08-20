00f8fb7d7a0eaabb996b450333661ad6b66457b1
ablacs <ablacsxd@gmail.com>
2025-08-20 10:32:59 -0300

    Testando o diretório

---
+++ aula02/estrutura_programa.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (){
+
+    printf("Hello World");
+    printf("----------------\n\n");
+    printf("N O T A  L E G A L\n");
+    printf("Carderno    1   10.00\n");
+    printf("Lápis       2   5.00\n");
+    printf("Borracha    1   7.50\n");
+    printf("%s %i %.2f\n", "Caderno",1,10.0);
+    printf("--------------------\n");
+    printf("Total ......:  22.50\n");
+    return 0;
+
+}
