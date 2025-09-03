4b7467ec44a9c129bd768395ec780cb8523b84ea
unknown <2522130018@ACADEMICO.IESB.BR>
2025-09-03 10:55:04 -0300

    terminando o exercício 10

--- aula03/declara_livro.c
+++ aula03/declara_livro.c
@@ -4,5 +4,9 @@ int main (){
     int Num = 000;
     float price = 0000.00;
     int year = 0000;
+    printf("ISBN: %i\n",ISBN);
+    printf("Num. Paginas: %i\n",Num);
+    printf("Preco: %.2f\n",price);
+    printf("Publicado em: %i\n",year);
     return 0;
 }
