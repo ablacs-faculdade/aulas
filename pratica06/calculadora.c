e5c8a5eead6bd7c7ba756264dfc1b12a0fc48d78
unknown <2522130018@academico.iesb.br>
2025-10-15 08:45:32 -0300

    Pratica06

--- pratica06/calculadora.c
+++ pratica06/calculadora.c
@@ -1,12 +1,39 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
 int main (){
-    char operacao;
-    printf("Digite a operacao: ");
-    scanf("%c", &operacao);
+    int operacao;
+    printf("Digite a operacao: 1 para soma, 2 para subtracao, 3 para divisao e 4 para multiplicacao ");
+    scanf("%i", &operacao);
     float numero1;
     float numero2;
-    if (operacao == "Somar" || operacao == "somar" || operacao == "adicao"){
-        printf("%f",numero1+numero2);
+    printf("Digite o primeiro numero: ");
+    scanf("%f",&numero1);
+    printf("Digite o segundo numero: ");
+    scanf("%f",&numero2);
+    switch (operacao)
+    {
+    case 1:
+        printf("%.2f + %.2f = %.2f",numero1,numero2,numero1+numero2);
+        break;
+    case 2:
+        printf("%.2f - %.2f = %.2f",numero1,numero2,numero1-numero2);
+        break;
+    case 3:
+        printf("%.2f / %.2f = %.2f",numero1,numero2,numero1/numero2);
+        break;
+    case 4:
+        printf("%.2f * %.2f = %.2f",numero1,numero2,numero1*numero2);
+        break;
+
+    
+    default:
+        printf("Operacao invalida");
+        break;
     }
+    
+
+    
     return 0;
 }
